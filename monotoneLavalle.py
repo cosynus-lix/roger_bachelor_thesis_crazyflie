21370191a2c2706050b927aef6a447811cd7301f
Alexis-BX <alexisroger99@gmail.com>
2020-02-25 17:26:05 +0100

    monotone La Valle

---
+++ monotoneLavalle.py
@@ -0,0 +1,64 @@
+import numpy as np
+
+def comp(a, b):
+    if len(a) != len(b):
+        return False
+    for a2, b2 in zip(a, b):
+        if a2 != b2:
+            return False
+    return True
+
+def monotoneLavalle(start, end, configSpace, lengths, possiblePath):
+    for point in possiblePath:
+        point = tuple([int(s) for s in point])
+        configSpace[point] = 2
+
+    addCriticals(configSpace, lengths)
+
+    start = [int(s) for s in start]
+    optiPath = [start]
+
+    while not comp(start, end):
+        i, j = start
+        if i+1>=lengths[0]:
+            j += 1
+            if configSpace[(i,j)]==1: 
+                print("non monotone path error")
+                break
+        elif j+1>=lengths[1]:
+            i += 1
+            if configSpace[(i,j)]==1: 
+                print("non monotone path error")
+                break
+        elif configSpace[(i+1,j+1)]!=1:
+            i += 1
+            j += 1
+        elif configSpace[(i+1,j)]!=1:
+            i += 1
+        elif configSpace[(i,j+1)]!=1:
+            j += 1
+        else:
+            print("Error, got stuck or at the end")
+        
+        optiPath.append((i,j))
+        start = (i,j)
+    return optiPath
+
+
+def addCriticals(configSpace, lengths):
+    for i, row in enumerate(configSpace):
+        if i==0: continue
+        afterPath = True
+        N = len(row)
+        for j in range(N)[::-1]:
+            if j==0: continue
+            value = row[j]
+            if value==1:
+                if afterPath:
+                    if configSpace[i-1, j] == 0:
+                        configSpace[i-1,j] = 1
+                else:
+                    if configSpace[i,j-1] == 0:
+                        configSpace[i,j-1] = 1
+            if value==2: 
+                afterPath = False
--- paretoConstPath.py
+++ paretoConstPath.py
@@ -2,20 +2,21 @@ from drone3D import Drone
 from nDimAstar import astar
 import numpy as np
 from time import sleep
+from monotoneLavalle import monotoneLavalle
 
 # 0: empty
 # 1: obstacle
 # 2: path
 # 3: drones
-nmap = [[[0,1,0],
-         [0,0,0],
-         [0,1,0]]]
+nmap = [[[0,1,1,0],
+         [0,0,0,0],
+         [0,1,1,0]]]
 
 start = (0,0,0)
-end = (2,2,0)
+end = (3,2,0)
 d1 = Drone(start, end, nmap)
 
-start = (2,0,0)
+start = (3,0,0)
 end = (0,2,0)
 d2 = Drone(start, end, nmap)
 
@@ -27,14 +28,38 @@ amount = len(drones)
 configSpace = np.zeros(lengths)
 
 # review for n drones
+# create config space for 2 drones
+toCheck = []
 for i, point in enumerate(paths[0]):
     for j, point2 in enumerate(paths[1]):
         if point == point2:
             configSpace[(i,j)] = 1
+            toCheck.append((i,j))
+
+# convexify it
+while len(toCheck)!=0:
+    p = toCheck.pop()
+    i, j = p
+    if i!=lengths[0] and j!=0:
+        if configSpace[(i+1,j-1)] == 1:
+            if configSpace[(i,j-1)] != 1:
+                configSpace[(i,j-1)] = 1
+                toCheck.append((i,j-1))
+
+
 
 start = np.zeros(amount)
 end = np.array(lengths)-np.ones(amount)
-paretoEfficient = astar(start, end, configSpace, lengths)
+
+method = "monotone"
+
+possiblePath = astar(start, end, configSpace, lengths)
+
+if method == "astar":
+    paretoEfficient = possiblePath
+elif method == "monotone":
+    #only implemented in 2D for now
+    paretoEfficient = monotoneLavalle(start, end, configSpace, lengths, possiblePath)
 
 # DEMO
 for i in range(len(paretoEfficient)):
@@ -55,3 +80,7 @@ for i in range(len(paretoEfficient)):
     sleep(0.5)
     nmap[0][int(p1[1])][int(p1[0])] = old1 
     nmap[0][int(p2[1])][int(p2[0])] = old2
+
+
+for line in configSpace[::-1]:
+    print(line)
