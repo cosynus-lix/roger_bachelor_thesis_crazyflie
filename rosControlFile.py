5c62cab6d38c069604f9f6599ab4458624493c0f
Alexis-BX <alexisroger99@gmail.com>
2020-02-28 17:09:32 +0100

    bon week-end

--- rosControlFile.py
+++ rosControlFile.py
@@ -15,14 +15,16 @@ from helpers import *
 
 # local position callback [if need]
 def local_position_callback(msg):
-	global current_position
-	current_position.x = msg.values[0]
-	current_position.y = msg.values[1]
-	current_position.z = msg.values[2]
-	current_position.header = msg.header
+    global current_positions
+    i = int(msg.header.frame_id[2])-1
+    current_positions[i].x = msg.values[0]
+    current_positions[i].y = msg.values[1]
+    current_positions[i].z = msg.values[2]
+    current_positions[i].header = msg.header
+    print(current_positions[i])
 
 def launchDrone(i, path, changeBasisConstants):
-    global current_position
+    global current_positions
     rospy.init_node('fancy_traj_node', anonymous=True)
 
     cf_prefix = "/cf"+str(i+1)
@@ -38,14 +40,14 @@ def launchDrone(i, path, changeBasisConstants):
     rate = rospy.Rate(freq_pub)
 
     # Initialize global variable for actual position of crazyflie
-    current_position = Position()
+    current_positions[i] = Position()
 
     # message used to store pos info to send
     next_pos = Position()
 
 
 	# Takeoff first -> go to 0, 0 , 1
-    while ((0-current_position.x)**2 + (0 - current_position.y)**2 + (1 - current_position.z)**2) > 1e-2 : # 10cm 
+    while d((0,0,1), current_positions[i]) > 1e-2 : # 10cm (as no root, rather 3cm?) 
         next_pos.x = 0
         next_pos.y = 0
         next_pos.z = 1
@@ -58,7 +60,7 @@ def launchDrone(i, path, changeBasisConstants):
     x, y, z = changeBasis(path[0], changeBasisConstants)
     
     # first go to the first point of the trajectory
-    while d((x,y,z), current_position) > 1e-2: # 10cm (as no root, rather 3cm?) 
+    while d((x,y,z), current_positions[i]) > 1e-2: # 10cm (as no root, rather 3cm?) 
         next_pos.x, next_pos.y, next_pos.z = x, y, z
         next_pos.yaw = 0.0
         next_pos.header.seq += 1
@@ -71,7 +73,7 @@ def launchDrone(i, path, changeBasisConstants):
     print("Starting path")
     for i, point in enumerate(path):
         gx, gy, gz = changeBasis(point, changeBasisConstants)
-        while d((gx,gy,gz), current_position) > 1e-2: # 10cm (as no root, rather 3cm?)
+        while d((gx,gy,gz), current_positions[i]) > 1e-2: # 10cm (as no root, rather 3cm?)
             next_pos.x, next_pos.y, next_pos.z = gx, gy, gz
             next_pos.yaw = 0.0
             next_pos.header.seq += 1
@@ -81,11 +83,11 @@ def launchDrone(i, path, changeBasisConstants):
     print("Done path")
 
     # Sleep for some time
-    for i in range(1000):
+    for i in range(100):
         rate.sleep()
 
     # Land -> go to 0 0 0.1
-    while d((0,0,0.1), current_position) > 1e-2: # 10cm (as no root, rather 3cm?)
+    while d((0,0,0.1), current_positions[i]) > 1e-2: # 10cm (as no root, rather 3cm?)
         next_pos.x, next_pos.y, next_pos.z = 0, 0, 0.1
         next_pos.yaw = 0.0
         next_pos.header.seq += 1
@@ -95,6 +97,7 @@ def launchDrone(i, path, changeBasisConstants):
     return
 
 if __name__ == '__main__':
+    global current_positions
     nmap = [[[0,0,0],
              [1,0,0],
              [1,0,0],
@@ -111,5 +114,7 @@ if __name__ == '__main__':
 
     changeBasisConstants = setConstants(bounds, (2.7,5.7,2.7))
 
+    current_positions = [Position() for _ in starts]
+# to hard combine
     for i, path in enumerate(paths):
         launchDrone(i, path, changeBasisConstants)
